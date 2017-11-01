4fc1300dcda4f11c2423936cd752d29146122f4b
LetsNolja <gaetan.schwr@edgue.ch>
2017-11-01 11:41:43 +0100

    yay

--- quiz/index.js
+++ quiz/index.js
@@ -1,7 +1,46 @@
 var questionFieldVue = new Vue({
   el: '#questionField',
   data: {
-    qs: []
+    qs: [],
+    ingame : false
+  },
+  methods: {
+    ctrlBtnHandler : function () {
+      console.log('Started quiz !')
+      let el = document.getElementById('controlBtn');
+      if (el.firstChild.data == "Start") {
+        this.startQ()
+        el.firstChild.data = "Stop";
+      } else {
+        this.stopQ()
+        el.firstChild.data = "Start";
+      }
+    },
+    startQ : function () {
+      console.log('Starting ...')
+      this.ingame = true
+      this.changeQ()
+    },
+    stopQ : function () {
+      console.log('Stopping ...')
+      this.ingame = false
+    },
+    changeQ : function () {
+      let q = this.qs[Math.floor(Math.random()*this.qs.length)];
+      console.log(q)
+      document.getElementById('textField').innerHTML = q.text
+      let bF = document.getElementById('btnField')
+      for (i in q.possibilities) {
+        let btn = document.createElement("BUTTON");        // Create a <button> element
+        let txt = document.createTextNode(q.possibilities[i]);       // Create a text node
+        btn.appendChild(txt);                       // Append the text to <button>
+        btn.onclick = () => {
+          console.log('Answered '+q.possibilities[i])
+        }
+        bF.appendChild(btn);
+        console.log(btn)
+      }
+    },
   }
 })
  questionFieldVue.qs = questions
