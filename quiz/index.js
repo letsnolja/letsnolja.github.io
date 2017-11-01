f4a3920fd193c54dcedc57d8d581e45420cc8cd2
Gaëtan <33280429+letsnolja@users.noreply.github.com>
2017-11-01 22:55:53 +0100

    Yay a commit

--- quiz/index.js
+++ quiz/index.js
@@ -1,46 +1,95 @@
+
+
 var questionFieldVue = new Vue({
   el: '#questionField',
   data: {
-    qs: [],
-    ingame : false
+    questions: getQuestions(),
+    ingame : false,
+    score : 0
   },
   methods: {
     ctrlBtnHandler : function () {
-      console.log('Started quiz !')
       let el = document.getElementById('controlBtn');
-      if (el.firstChild.data == "Start") {
-        this.startQ()
+      if (!this.ingame) {
+        this.startQuiz()
         el.firstChild.data = "Stop";
       } else {
-        this.stopQ()
+        this.stopQuiz()
         el.firstChild.data = "Start";
       }
     },
-    startQ : function () {
-      console.log('Starting ...')
+    startQuiz : function () {
+      console.log('[*] Started Quiz')
+      this.score = 0
       this.ingame = true
-      this.changeQ()
+      this.changeQuestion()
     },
-    stopQ : function () {
-      console.log('Stopping ...')
+    stopQuiz : function () {
+      console.log('[*] Stopped Quiz')
+      this.cleanTextField()
+      this.cleanBtnField()
+      this.questions = getQuestions()
+      this.score = 0
       this.ingame = false
     },
-    changeQ : function () {
-      let q = this.qs[Math.floor(Math.random()*this.qs.length)];
-      console.log(q)
-      document.getElementById('textField').innerHTML = q.text
-      let bF = document.getElementById('btnField')
-      for (i in q.possibilities) {
-        let btn = document.createElement("BUTTON");        // Create a <button> element
-        let txt = document.createTextNode(q.possibilities[i]);       // Create a text node
-        btn.appendChild(txt);                       // Append the text to <button>
-        btn.onclick = () => {
-          console.log('Answered '+q.possibilities[i])
-        }
-        bF.appendChild(btn);
-        console.log(btn)
+    cleanTextField : function () {
+      let tF = document.getElementById('textField')
+      while (tF.firstChild) {
+        tF.removeChild(tF.firstChild);
       }
     },
+    cleanBtnField : function () {
+      let bF = document.getElementById('btnField')
+      while (bF.firstChild) {
+        bF.removeChild(bF.firstChild);
+      }
+    },
+    changeQuestion : function () {
+      if (this.questions.length == 0) {
+        this.finishedQuiz()
+        return
+      }
+      questionIndex = Math.floor(Math.random()*this.questions.length)
+      let q = this.questions[questionIndex]
+      this.questions.splice(questionIndex, 1)
+      document.getElementById('textField').innerHTML = q.text
+      // Dynamically generate buttons
+      let bF = document.getElementById('btnField')
+      this.cleanBtnField()
+      for (i in q.possibilities) {
+        let btn = document.createElement("BUTTON");
+        let txt = document.createTextNode(q.possibilities[i]);
+        btn.appendChild(txt);
+        // Set up onclick with custom generated functions (currying)
+        btn.id = 'choice_'+i
+        btn.className = 'class="btn btn--raised'
+        btn.style.position = 'relative'
+        btn.onclick = this.answeredWrapper(q,this)
+        console.log(btn)
+        bF.appendChild(btn);
+        bF.appendChild(document.createElement("BR"))
+      }
+    },
+    finishedQuiz : function () {
+      this.cleanBtnField()
+      let tF = document.getElementById('textField')
+      tF.innerHTML = 'Congrats you got <b>'+this.score+'</b>'
+    },
+    answeredWrapper : function (q, vueInstance) {
+      answered = function () {
+        if (this.textContent == q.answer) {
+          console.log(this.textContent+' is right !')
+          vueInstance.score ++
+          vueInstance.changeQuestion()
+
+        } else {
+          console.log(this.textContent+' is wrong !')
+          this.style.visibility = 'hidden';
+          vueInstance.score --
+        }
+        console.log(vueInstance.score)
+      }
+      return answered
+    }
   }
 })
- questionFieldVue.qs = questions
