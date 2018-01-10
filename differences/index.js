ef00e5f88b35ecfe6feb2ad6a4ea82ef2823f228
eleve <eleve@sismondi-ce13-09.local>
2018-01-10 10:27:37 +0100

    I don't care if my gramar shit is

--- differences/index.js
+++ differences/index.js
@@ -1,4 +1,25 @@
 var app = new Vue({
   el: '#app',
-  data: {}
+  data: {
+    dialog : false,
+    dialog2 : false,
+    dialog3 : false,
+    dialog4 : false,
+    dialog5 : false,
+    dialog6 : false,
+    showMenu : false,
+    x : 0,
+    y : 0
+  },
+  methods: {
+      show (e) {
+        e.preventDefault()
+        this.showMenu = false
+        this.x = e.clientX
+        this.y = e.clientY
+        this.$nextTick(() => {
+          this.showMenu = true
+        })
+      }
+    }
 })
