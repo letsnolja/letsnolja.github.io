2db44676a904ce4b7bea735ea32124b6f8156fb6
eleve <eleve@sismondi-ce13-09.local>
2018-01-10 10:27:37 +0100

    Hello

--- differences/index.js
+++ differences/index.js
@@ -1,4 +1,11 @@
 var app = new Vue({
   el: '#app',
-  data: {}
+  data: {
+    dialog : false,
+    dialog2 : false,
+    dialog3 : false,
+    dialog4 : false,
+    dialog5 : false,
+    dialog6 : false
+  }
 })
